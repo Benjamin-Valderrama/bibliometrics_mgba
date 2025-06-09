43b1c880e5cde050f5a678a740ebab4294a30db3
Benjamin-Valderrama <bvalderramabobadilla@gmail.com>
2025-06-09 21:58:36 +0100

    add custom ggplot theme

--- scripts/functions.R
+++ scripts/functions.R
@@ -1,5 +1,18 @@
 #' Functions that can be helpful
 
+theme_bv <- function(){
+        
+    theme_bw() +
+    theme(
+          axis.title = element_text(color = "black"),
+          axis.text = element_text(color = "black"),
+          
+          panel.border = element_rect(color = "black")
+          
+          )
+        
+}
+
 read_biblio <- function(path){
     path %>% 
         read_xlsx(sheet = "savedrecs") %>% 
