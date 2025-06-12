5df840e67169e9595d5687f8b0a7b76272ad3aaa
Benjamin-Valderrama <bvalderramabobadilla@gmail.com>
2025-06-12 21:38:10 +0100

    new figure 1 + text and code to make it

--- readme.md
+++ readme.md
@@ -24,13 +24,21 @@ An semi-automated bibliometric analysis of the publications made on the Microbio
 
 ## Introduction
 
-I'm using this published work by [(Wang et al. 2020)](https://pmc.ncbi.nlm.nih.gov/articles/PMC9119018/) as a reference.
+A previous article performed a bibliometrics analysis of the Microbiome-Gut-Brain axis field from 2004 to 2020 [(Wang et al. 2020)](https://pmc.ncbi.nlm.nih.gov/articles/PMC9119018/). Although informative upon publication, the analysis is not longer reflective of the current status of the field, which grows every year. To overcome this limitation, we conducted a new bibliometric analysis here introduced as a live article [REF].
+
+Additionally, previous research has been shown that current microbiome research is skewed towards an overrepresentation of European and North American populations [(Abdill et al. 2022)](https://journals.plos.org/plosbiology/article?id=10.1371/journal.pbio.3001536). Although no exploration has been conducted specifically on the Microbiome-Gut-Brain axis field, I have no reasons to think it would be different.
 
 ## Results and Discussion
 
-First, we wanted to explore the country-level collaboration network. We first identified the top three most cited countries: China (24,952 total citations; 30.4 citations per article on average), Ireland (19457 total citations, 155.7 citations per article on average) and USA (15,568 total citations, 49.7 citations per article on average). Then, we estimated the country-level collaboration networks based on the co authorship across papers in the database. We then plotted the collaboration of the top three most cited countries with other countries from other world regions: Asia (Figure 1A), Europe (Figure 1B), Africa (Figure 1C) and South America (Figure 1D).
+#### Top cited authors and countries, and who they collaborate with
 
-![Collaboration networks between the 3 most productive countries and other world regions. (A) Asia (B) Europe (C) Africa (D) South America. The top 3 most productive countries (China, Ireland and USA) are shown in red. Other countries in grey.](outputs/mains/country_collab.jpg)
+We wanted to explore large temporal patterns in the Microbiome-Gut-Brain axis field. We first determined the 10 most cited authors (Figure 1A) and the 8 most cited countries (Figure 1B) across all years. There is a clear stability in terms of the most cited authors (Figure 1A), as the top 3 are highly cited across all years. Interestingly, among the most cited authors there are some that were highly cited in one year but their citations drastically dropped after. Regarding the top cited countries, China and the USA are dominating the ranking in the last couple of years as the most and second most cited countries in the field, respectively. Interestingly, although Ireland was in the top 3 most cited countries for more than a decade (Figure 1B), the country is moving down in the ranking to an historic low. In contrast, research from Australia and Belgium have gain citations in the same period.
+
+Next, we wanted to explore the collaboration network of the most influential countries and authors of the field. We selected the most cited countries in the last three years: China (24,952 total citations; 30.4 citations per article on average), Ireland (19,457 total citations, 155.7 citations per article on average) and USA (15,568 total citations, 49.7 citations per article on average). Then, we determined the country-level collaboration networks based on the co authorship across papers in the database. We then plotted the collaboration networks of these most cited countries, with emphasis in how they collaborate across world regions: Asia (Figure 1C), Europe (Figure 1D), Africa (Figure 1E) and South America (Figure 1F).
+
+Not that the top three most cited countries have connections with countries from all world regions. Interestingly, whereas many local collaborations among countries in Asia and Europe (Figure 1C and 1D) were found, local collaborations among African and South American countries (Figure 1E and 1F) are less common.
+
+![**Figure 1: Most cited authors, countries and their collaboration networks.** (A) Top 10 most cited authors over time. Shades of orange show the number of citations per year at the moment of screening. (B) Ranking of the top 8 most cited countries over time. The cumulative number of citations is shown for each year. Each country is a color. (C) collaboration networks of the 3 most cited countries and countries from Asia. (B) Same as A, but with countries from Europe (C) Africa (D) South America. The top 3 most productive countries (China, Ireland and USA) are shown in red. Other countries in grey.](outputs/mains/figure1.jpg)
 
 ## Strenghts and Limitations
 
@@ -48,8 +56,10 @@ The data was collected using the [Web of science (WoS) search tool](https://www-
 
 The data was analysed using the [bibliometrix](https://www.bibliometrix.org/home/) package. Networks were ploted using the [ggnet2](https://briatte.github.io/ggnet/#:~:text=The%20ggnet2%20function%20is%20a,one%2Dmode%20igraph%20network%20objects.) package.
 
-All code used in the analysis and to produce the figures is publicly available in the script: 'scripts/analysis.R'. Custom functions used to facilitate the analysis can be found in 'scripts/functions.R'.
+The code used in the analysis and in the generation of figures is publicly available in the script: 'scripts/analysis.R'. Custom functions used to facilitate the analysis can be found in 'scripts/functions.R'.
 
 ## References
+
+[Abdill RJ, Adamowicz EM, Blekhman R (2022) Public human microbiome data are dominated by highly developed countries. PLOS Biology 20(2): e3001536. doi: 10.1371/journal.pbio.3001536](https://journals.plos.org/plosbiology/article?id=10.1371/journal.pbio.3001536)
 
 [Wang H, Long T, You J, Li P, Xu Q. Bibliometric Visualization Analysis of Microbiome-Gut-Brain Axis from 2004 to 2020. Med Sci Monit. 2022 May 15;28:e936037. doi: 10.12659/MSM.936037. PMID: 35568968; PMCID: PMC9119018.](https://pmc.ncbi.nlm.nih.gov/articles/PMC9119018/)
--- scripts/analysis.R
+++ scripts/analysis.R
@@ -41,41 +41,45 @@ authors_df <- biblio %>%
     summarise(publications = n(),
               citations = sum(TC, na.rm = TRUE),
               .by = c(PY, AU)) %>% 
-    filter(publications > 2) %>% 
     # add columns with total pubs (across years) by author 
     mutate(total_citations = sum(citations), 
            total_publications = sum(publications), .by = AU) %>% 
     # add column for the axis of the plot
-    mutate(AU_w_details = paste0(AU, "\n(Total cits:", total_citations, "\n",
-                                 "Total pubs:", total_publications, ")"))
+    mutate(AU_w_details = paste0(AU, "\n(Total cits:", total_citations, ")"))
 
-top_authors_ordered <- authors_df %>% 
-    select(total_citations, AU_w_details) %>% 
-    # because we used mutate in 'authors_df', we need to remove the repeated rows
+top_cited_authors_ordered <- authors_df %>% 
+    select(AU, AU_w_details, total_citations) %>% 
     unique() %>% 
     slice_max(n = 10, order_by = total_citations, with_ties = TRUE) %>% 
     pull(AU_w_details)
 
-# results as heatmap
-top_authors_df <- authors_df %>% 
-    filter(AU_w_details %in% top_authors_ordered) %>% 
-    mutate(AU_w_details = factor(AU_w_details, 
-                                 levels = rev(top_authors_ordered)))
 
+top_cited_authors_df <- authors_df %>% 
+    filter(AU_w_details %in% top_cited_authors_ordered) %>% 
+    select(AU_w_details, PY, citations) %>% 
+    # pivot wider and longer to fill all years where authors didn't publish
+    # using a 0
+    pivot_wider(names_from = PY, values_from = citations, 
+                values_fill = 0) %>% 
+    pivot_longer(cols = starts_with("20"),
+                 names_to = "PY", values_to = "citations") %>% 
+    mutate(PY = as.numeric(PY)) %>% 
+    mutate(AU_w_details = factor(AU_w_details, 
+                                 levels = rev(top_cited_authors_ordered)))
+
+
+# automate the fill scale for the heat map
 breaks <- seq(from = 1e3,
-              to = (round(max(top_authors_df$citations)/1e3)) * 1e3,
+              to = (round(max(top_cited_authors_df$citations)/1e3)) * 1e3,
               by = 1e3)
+
 low_range <- paste0(c(breaks[1:length(breaks) -1] - 999), "-")
 low_range <- c(low_range, ">")
 labels <- paste0(low_range, breaks)
 
 
-plot_top_cited_authors <- top_authors_df %>% 
-    pivot_wider(id_cols = c(AU, AU_w_details, total_citations, total_publications),
-                names_from = PY, values_from = citations, values_fill = 0) %>% 
-    pivot_longer(cols = starts_with("20"),
-                 names_to = "PY", values_to = "citations") %>% 
-    mutate(PY = as.numeric(PY)) %>% 
+# plot top cited authors
+plot_top_cited_authors <- top_cited_authors_df %>% 
     ggplot(aes(x = PY, y = AU_w_details)) +
     
     geom_tile(aes(fill = citations), color = "black") +
@@ -109,7 +113,7 @@ plot_top_cited_authors <- top_authors_df %>%
     theme_bv() +
     theme(panel.grid.major = element_blank()); plot_top_cited_authors
 
-ggsave(file = "outputs/top_authors.jpg", 
+ggsave(file = "outputs/top_cited_authors.jpg", 
        plot = plot_top_cited_authors,
        height = 6, width = 7)
 
@@ -118,39 +122,41 @@ ggsave(file = "outputs/top_authors.jpg",
 # Most cited countries over time ------------------------------------------
 
 # get country info for each author
-countries_df <- metaTagExtraction(biblio, Field = "AU_CO", sep = ";")
-
-# papers published for each country across all years
-countries_by_year <- countries_df %>% 
+countries_df <- metaTagExtraction(biblio, Field = "AU_CO", sep = ";") %>% 
     select(UT, AU_CO, TC, PY) %>% 
     separate_longer_delim(cols = AU_CO, delim = ";") %>% 
+    # remove duplicated rows (authors from same nationality in the same paper)
     unique() %>% 
     summarise(publications = n(),
               citations = sum(TC, na.rm = TRUE),
               .by = c(PY, AU_CO)) %>% 
     filter(!is.na(AU_CO))
 
-# get years to plot
-valid_years <- countries_by_year %>% 
+
+# only plot years with more than 3 countries publishing in them
+years_to_plot <- countries_df %>% 
     summarise(total_countries = n(), .by = PY) %>% 
     filter(total_countries > 3) %>% 
     pull(PY)
 
-# get countries to plot
-top_countries <- countries_by_year %>% 
-    filter(PY %in% valid_years) %>% 
+# get the 8 most cited countries to plot
+countries_to_plot <- countries_df %>% 
+    filter(PY %in% years_to_plot) %>% 
     summarise(total_citations = sum(citations), .by = AU_CO) %>% 
     slice_max(n = 8, order_by = total_citations) %>% 
     pull(AU_CO)
 
 # dataframe to plot
-top_countries_df <- countries_by_year %>% 
-    filter(PY %in% valid_years) %>% 
-    filter(AU_CO %in% top_countries) %>% 
-    mutate(rank = rank(citations, ties.method = "max"), .by = PY) %>% 
-    mutate(AU_CO = factor(x = AU_CO, levels = rev(top_countries)))
+top_cited_countries_df <- countries_df %>% 
+    filter(PY %in% years_to_plot) %>% 
+    filter(AU_CO %in% countries_to_plot) %>% 
+    arrange(PY) %>% 
+    mutate(cum_citations = cumsum(citations), .by = "AU_CO") %>% 
+    mutate(rank = rank(-citations, ties.method = "max"), .by = PY) %>% 
+    mutate(AU_CO = factor(x = AU_CO, levels = countries_to_plot))
 
-plot_top_cited_countries <- top_countries_df %>% 
+
+plot_top_cited_countries <- top_cited_countries_df %>% 
     
     ggplot(aes(x = PY, y = rank, color = AU_CO)) +
     
@@ -159,17 +165,15 @@ plot_top_cited_countries <- top_countries_df %>%
     geom_point(size = 4.5, alpha = 0.8) +
     geom_point(size = 1.5, color = "#fffff0") +
     
-    scale_y_continuous(limits = c(1, 8),
-                       breaks = seq(1, 8, by = 1),
-                       labels = rev(seq(1, 8, by = 1))) +
-    
-    scale_x_continuous(limits = c(min(valid_years),
-                                  max(valid_years)),
+    scale_x_continuous(limits = c(min(years_to_plot),
+                                  max(years_to_plot)),
                        
-                       breaks = seq(min(valid_years),
-                                    max(valid_years),
-                                    by = 2)
-    ) +
+                       breaks = seq(min(years_to_plot),
+                                    max(years_to_plot),
+                                    by = 2)) +
+    
+    scale_y_reverse(breaks = seq(1, 8, by = 1)) +
+    
     labs(x = "Year",
          y = "Most cited countries (Ranking)",
          color = "Country") +
@@ -179,7 +183,7 @@ plot_top_cited_countries <- top_countries_df %>%
     theme(); plot_top_cited_countries
 
 
-ggsave(file = "outputs/top_countries.jpg", 
+ggsave(file = "outputs/top_cited_countries.jpg", 
        plot = plot_top_cited_countries,
        height = 5, width = 6)
 
@@ -193,9 +197,9 @@ ggsave(file = "outputs/top_countries.jpg",
 field_analysis <- biblioAnalysis(biblio); field_analysis
 field_summary <- summary(field_analysis); field_summary
 
-# collaboration network of those countries
-M <- metaTagExtraction(biblio, Field = "AU_CO", sep = ";")
-net_matrix <- biblioNetwork(M,
+# collaboration network using all countries
+biblio_w_country <- metaTagExtraction(biblio, Field = "AU_CO", sep = ";")
+net_matrix <- biblioNetwork(biblio_w_country,
                             analysis = "collaboration", 
                             network = "countries", sep = ";")
 
@@ -215,11 +219,9 @@ plots_country_collab_graphs <- df_plots %>%
     filter(continent %in% c("AFRICA", "EUROPE", "SOUTH AMERICA", "ASIA")) %>% 
     pull(collab_plot) %>% 
     wrap_plots() + 
-    plot_layout(nrow = 2) +
-    plot_annotation(tag_levels = "A") &
-    theme(plot.tag = element_text(size = 20, face = "bold")); plots_country_collab_graphs
+    plot_layout(nrow = 2, heights = c(3, 1)); plots_country_collab_graphs
 
-ggsave(filename = "outputs/mains/country_collab.jpg",
+ggsave(filename = "outputs/country_collab_networks.jpg",
        plot = plots_country_collab_graphs,
        height = 8, width = 16)
 
@@ -228,9 +230,13 @@ ggsave(filename = "outputs/mains/country_collab.jpg",
 
 # Figure 1 ----------------------------------------------------------------
 
-plot_top_cited_authors + plot_top_cited_countries + 
+(plot_top_cited_authors + plot_top_cited_countries) /
+    plots_country_collab_graphs +
+    plot_layout(heights = c(1, 2)) +
     plot_annotation(tag_levels = "A") &
-    theme(plot.tag = element_text(size = 20, face = "bold"))
+    theme(plot.tag = element_text(size = 32, face = "bold"),
+          axis.text = element_text(size = 12),
+          text = element_text(size = 16))
 
-ggsave(filename = "outputs/mains/most_cited_authors_and_countries.jpg",
-       height = 6, width = 14)
+ggsave(filename = "outputs/mains/figure1.jpg",
+       height = 18, width = 18)
